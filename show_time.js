c8476bbd1023f77a6efb949617491551750e131d
Kris <knnordseth@.com>
2022-11-15 13:43:51 +0100

    updated port to ip

--- show_time.js
+++ show_time.js
@@ -2,7 +2,7 @@ window.addEventListener("DOMContentLoaded", () => {
  const messages = document.createElement("ul");
  document.body.appendChild(messages);
 
-  const websocket = new WebSocket("ws://localhost:5678/");
+  const websocket = new WebSocket("ws://51.120.2.255:5678/");
      websocket.onmessage = ({ data }) => {
     const message = document.createElement("li");
     const content = document.createTextNode(data);
